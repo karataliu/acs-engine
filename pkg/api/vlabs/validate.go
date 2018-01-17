8963ab835958c1a9c0f157da8f8ab1fd355aa9d2
Dong Liu <doliu@microsoft.com>
2018-01-17 09:58:41 +0800

    Enable cloud controller manager support for 1.9

--- pkg/api/vlabs/validate.go
+++ pkg/api/vlabs/validate.go
@@ -632,6 +632,8 @@ func (a *KubernetesConfig) Validate(k8sVersion string) error {
 		common.KubernetesVersion1Dot8Dot2: true,
 		common.KubernetesVersion1Dot8Dot4: true,
 		common.KubernetesVersion1Dot8Dot6: true,
+		common.KubernetesVersion1Dot9Dot0: true,
+		common.KubernetesVersion1Dot9Dot1: true,
 	}
 
 	if a.UseCloudControllerManager != nil && *a.UseCloudControllerManager || a.CustomCcmImage != "" {
